d5e2101dd105a3209435e22e94c5b775798d4c65
Spencer <spencercetinic@hotmail.com>
2020-12-16 22:55:17 -0500

    [Grid] Add initial sparse grid and templates, wireframed basic simulation outline

--- include/grid.h
+++ include/grid.h
@@ -2,6 +2,7 @@
 // Created by behrooz on 12/16/20.
 //
 
+
 #ifndef INC_2D_GRID_H
 #define INC_2D_GRID_H
 
@@ -9,108 +10,210 @@
 #include <visualization.h>
 
 namespace grid{
-
-    class Grid{
-    public:
-        Grid() = default;
-        ~Grid() = default;
+    /*
+     * Different types for the cells inside a macgrid
+     */
+    enum CellTypes {
+      SOLID,
+      FLUID,
+      AIR
     };
 
-    class GridDense2D: Grid{
+
+    /*
+     * Data structure to represent macgrid of fluid simulation.
+     *
+     * EigenVector -> [Eigen::Vector2d || Eigen::Vector3d]
+     */
+    template <class EigenVector>
+    class MacGrid {
+      /*
+       * Struct containing data for a given cell of the macgrid
+       */
+      struct Cell {
+        CellTypes type;
+        int layer;
+        std::vector<Cell> neighbours;
+        EigenVector v;
+        EigenVector tmpV;
+
+        Cell() {
+          this->neighbours.reserve(8);
+        }
+      };
+
+      static const bool eq(const EigenVector& lhs, const EigenVector& rhs){ return lhs == rhs; };
+      static const std::size_t hash (const EigenVector & n){ return (std::size_t)(n*Eigen::Vector3d{541 + 79 + 31});};
+      typedef std::unordered_map<EigenVector, Cell, decltype(hash), decltype(eq)> SparseGrid;
+
     public:
-        GridDense2D() = default;
-        ~GridDense2D() = default;
+      MacGrid(int h, EigenVector o) :
+          height(h),
+          origin(o)
+      {}
 
-        /*
-         * @brief initialize the grids inside the containers
-         * @param container_x_size the container size in x direction
-         * @param container_y_size the container size in y direction
-         * @param number of grid cell in x direction
-         * @param number of grid cell in y direction
-         */
-        void init_2d_grid(double container_x_min,
-                          double container_x_max,
-                          double container_y_min,
-                          double container_y_max,
-                          int num_cell_x, int num_cell_y){
-            this-> container_x_max = container_x_max;
-            this-> container_x_min = container_x_min;
-            this-> container_y_max = container_y_max;
-            this-> container_y_min = container_y_min;
-            this-> Nx = num_cell_x;
-            this-> Ny = num_cell_y;
-            this-> N = Nx * Ny;
-            this-> pressure.resize(Nx * Ny);
-            this-> pressure.setZero();
-            this-> ux.resize((Nx + 1) * Ny);
-            this-> ux.setZero();
-            this-> uy.resize(Nx * (Ny + 1));
-            this-> uy.setZero();
-            this->cell_size_x = (container_x_max - container_x_min) / Nx;
-            this->cell_size_y = (container_y_max - container_y_min) / Ny;
-            //Defining Flags
-            this->fluid_cell.resize(Nx * Ny);
-            this->air_cell.resize(Nx * Ny);
-            this->solid_cell.resize(Nx * Ny);
-        }
+      typename SparseGrid::iterator begin() {
+        return this->sparseGrid.begin();
+      }
 
-        /*
-         * Show the grid inside the viewer window
-         */
-        void show_grid(igl::opengl::glfw::Viewer &Viewer){
-            // Horizontal grid lines
-            for (unsigned y=1; y < Ny; y++){
-                Viewer.data().add_edges(
-                        Eigen::RowVector3d(container_x_min,
-                                           y * cell_size_y + container_y_min,
-                                           0),
-                        Eigen::RowVector3d(container_x_max,
-                                           y * cell_size_y + container_y_min,
-                                           0),
-                        Eigen::RowVector3d(0,255,0)
-                );
-            }
-            // Vertical grid lines
-            for (unsigned x=1; x < Nx; x++){
-                Viewer.data().add_edges(
-                        Eigen::RowVector3d(x * cell_size_x + container_x_min,
-                                           container_y_min,
-                                           0),
-                        Eigen::RowVector3d(x * cell_size_x + container_x_min,
-                                           container_y_max,
-                                           0),
-                        Eigen::RowVector3d(0,255,0)
-                );
-            }
+      typename SparseGrid::iterator end() {
+        return this->sparseGrid.end();
+      }
+
+      bool containsParticle(EigenVector& e) {
+        return this->sparseGrid.count(e) > 0;
+      }
+
+      bool containsCell(Cell& c) {
+        return this->sparseGrid.count(c.origin);
+      }
+
+      Cell getCellFromParticle(EigenVector& e) {
+
+      }
+
+      EigenVector interpolateVelocity(EigenVector& e) {
+        EigenVector ne;
+        for (int i = 0; i < e.size(); i++) {
+          ne = (e / this->height) - (EigenVector::Identity() * 0.5);
+          ne(i) += 0.5;
+          ne(i) = this->_calculateInterpretedVelocity(ne,i);
         }
-        // Functions to work with MAC variables
+      }
+
+      double _calculateInterpretedVelocity(EigenVector ne, int index) {
+        if (ne.size() == 3) {
+          int i = std::floor(ne(0));
+          int j = std::floor(ne(1));
+          int k = std::floor(ne(2));
+
+          // @TODO - finish interpreted velocity
+          return 0.0;
+        } else if (ne.size() == 2) {
+
+        }
+      }
+
+      std::vector<Cell> generateNeighboursFromCell(Cell& c) {
+
+      }
+
+      Eigen::Ref<Eigen::VectorXd> getParticles() {
+        return this->markers;
+      }
+
+      Cell operator[](EigenVector& e) {
+        return sparseGrid[e];
+      }
 
     private:
-        //Number of grids in each direction
-        int Nx;
-        int Ny;
-        int N;
-        //Size of grids in each direction
-        double cell_size_x;
-        double cell_size_y;
-
-        //Container Coordination
-        double container_x_min;
-        double container_x_max;
-        double container_y_min;
-        double container_y_max;
-
-        //Flags
-        std::vector<bool> fluid_cell;
-        std::vector<bool> air_cell;
-        std::vector<bool> solid_cell;
-        //The variables of MAC grids
-        Eigen::VectorXd pressure;
-        Eigen::VectorXd ux;
-        Eigen::VectorXd uy;
-        Eigen::VectorXd uv;
+      Eigen::VectorXd markers;
+      EigenVector origin;
+      double height{};
+      SparseGrid sparseGrid;
     };
 
-    GridDense2D grid2d;
+class Grid {
+public:
+  Grid() = default;
+  ~Grid() = default;
+};
+
+class GridDense2D: Grid{
+public:
+  GridDense2D() = default;
+  ~GridDense2D() = default;
+
+  /*
+   * @brief initialize the grids inside the containers
+   * @param container_x_size the container size in x direction
+   * @param container_y_size the container size in y direction
+   * @param number of grid cell in x direction
+   * @param number of grid cell in y direction
+   */
+  void init_2d_grid(double container_x_min,
+                    double container_x_max,
+                    double container_y_min,
+                    double container_y_max,
+                    int num_cell_x, int num_cell_y){
+    this-> container_x_max = container_x_max;
+    this-> container_x_min = container_x_min;
+    this-> container_y_max = container_y_max;
+    this-> container_y_min = container_y_min;
+    this-> Nx = num_cell_x;
+    this-> Ny = num_cell_y;
+    this-> N = Nx * Ny;
+    this-> pressure.resize(Nx * Ny);
+    this-> pressure.setZero();
+    this-> ux.resize((Nx + 1) * Ny);
+    this-> ux.setZero();
+    this-> uy.resize(Nx * (Ny + 1));
+    this-> uy.setZero();
+    this->cell_size_x = (container_x_max - container_x_min) / Nx;
+    this->cell_size_y = (container_y_max - container_y_min) / Ny;
+    //Defining Flags
+    this->fluid_cell.resize(Nx * Ny);
+    this->air_cell.resize(Nx * Ny);
+    this->solid_cell.resize(Nx * Ny);
+  }
+
+  /*
+   * Show the grid inside the viewer window
+   */
+  void show_grid(igl::opengl::glfw::Viewer &Viewer){
+    // Horizontal grid lines
+    for (unsigned y=1; y < Ny; y++){
+      Viewer.data().add_edges(
+          Eigen::RowVector3d(container_x_min,
+                             y * cell_size_y + container_y_min,
+                             0),
+          Eigen::RowVector3d(container_x_max,
+                             y * cell_size_y + container_y_min,
+                             0),
+          Eigen::RowVector3d(0,255,0)
+      );
+    }
+    // Vertical grid lines
+    for (unsigned x=1; x < Nx; x++){
+      Viewer.data().add_edges(
+          Eigen::RowVector3d(x * cell_size_x + container_x_min,
+                             container_y_min,
+                             0),
+          Eigen::RowVector3d(x * cell_size_x + container_x_min,
+                             container_y_max,
+                             0),
+          Eigen::RowVector3d(0,255,0)
+      );
+    }
+  }
+  // Functions to work with MAC variables
+
+private:
+  //Number of grids in each direction
+  int Nx;
+  int Ny;
+  int N;
+  //Size of grids in each direction
+  double cell_size_x;
+  double cell_size_y;
+
+  //Container Coordination
+  double container_x_min;
+  double container_x_max;
+  double container_y_min;
+  double container_y_max;
+
+  //Flags
+  std::vector<bool> fluid_cell;
+  std::vector<bool> air_cell;
+  std::vector<bool> solid_cell;
+  //The variables of MAC grids
+  Eigen::VectorXd pressure;
+  Eigen::VectorXd ux;
+  Eigen::VectorXd uy;
+  Eigen::VectorXd uv;
+};
+
+GridDense2D grid2d;
 }
 #endif //INC_2D_GRID_H
--- include/simulation.h
+++ include/simulation.h
@@ -14,16 +14,21 @@
 
 
 namespace simulation{
-    class Sim{
+    template <class EigenVector>
+    class Sim {
     public:
-        Sim() = default;
+        Sim() {
+          this->macGrid(3,Eigen::Vector2d::Identity());
+        }
         ~Sim() = default;
         virtual void simulate_one_step(Eigen::MatrixXd &q, Eigen::MatrixXd &qdot, Eigen::VectorXd& forces,
                                        grid::GridDense2D& grid) {};
     private:
+      const grid::MacGrid<EigenVector> macGrid;
     };
 
-    class GeneralSimulation: public Sim{
+    template <class EigenVector>
+    class GeneralSimulation : public Sim<EigenVector> {
     public:
         /*
          * @brief we use this constructor to work with global variables
@@ -40,7 +45,7 @@ namespace simulation{
          * @param geometry_id: The pair vector of (id, description) of the id
          * of the obstacle in the scene with its definition
          */
-        void init_simulation(double dt, Eigen::Vector3d g, double density, double viscosity,
+        void init_simulation(double dt, EigenVector g, double density, double viscosity,
                              std::vector<std::pair<Eigen::MatrixXd, Eigen::MatrixXi>> geometry,
                              std::vector<std::pair<int, std::string>> geometry_id){
             this->dt = dt;
@@ -55,32 +60,140 @@ namespace simulation{
          */
         void simulate_one_step(Eigen::MatrixXd &q, Eigen::MatrixXd &qdot, Eigen::VectorXd& forces,
                                grid::GridDense2D& grid) override {
-            this->advect(q, qdot);
-            for(int prtc = 0; prtc < q.rows(); prtc++){
-                Eigen::Vector3d prtc_q = q.row(prtc);
-                Eigen::Vector3d prtc_qdot = qdot.row(prtc);
-                // Advect
-                prtc_q += prtc_qdot * dt;
-                // External Forces
-                prtc_qdot += gravity * dt;
-                q.row(prtc) = prtc_q;
-                qdot.row(prtc) = prtc_qdot;
+          this->advect(q, qdot);
+          for (int prtc = 0; prtc < q.rows(); prtc++){
+            Eigen::Vector3d prtc_q = q.row(prtc);
+            Eigen::Vector3d prtc_qdot = qdot.row(prtc);
+
+            // 1) Advect
+            prtc_q += prtc_qdot * dt;
+
+            // 2) Update grid
+            this->updateGrid();
+
+            // 3a) Adject velocity field
+            this->adjectVelocityField();
+
+            // Apply external forces to cells that border fluid cells
+            prtc_qdot += gravity * dt;
+            q.row(prtc) = prtc_q;
+            qdot.row(prtc) = prtc_qdot;
+
+            // Apply viscosity term
+
+            // Calculate pressure
+
+            // Apply pressure
+
+            // Extrapolate fluid velocities into surrounding cells
+            for (auto& cell : this->macGrid) {
+              cell.layer = cell.type == grid::CellTypes::FLUID ? 0 : -1;
             }
+            for (int i = 1; i < std::max(2, kfcl); i++) {
+              for (auto& cell : this->macGrid) {
+                if (cell.layer != -1)
+                  continue;
+                // if cell has neighbour with layer == i-1
+              }
+            }
+
+            // Set the velocities that point to solid cells
+            for (auto& cell : this->macGrid) {
+            }
+
+            // Move the marker particles through the fluid
+            for (auto& p : this->particles) {
+              // Determine if frame should be rendered
+              if (this->time % this->fps) {
+                p.p = p.p + p.v * this->dt;
+              } else {
+                // RK4 Method
+              }
+            }
+          }
         }
-    private:
-        Eigen::Vector3d gravity;
-        double dt;
-        double density;
-        double vis;
-        // Simulation helper functions
-        inline void advect(Eigen::MatrixXd &q, Eigen::MatrixXd &qdot){
+
+      /*
+       * Perform global update to macgrid prior to calculations
+       */
+      void updateGrid() {
+        // Grid update
+        auto particles = this->macGrid.getParticles();
+        auto numElm = particles.size() / this->macGrid.numParticles;
+        for (int i = 0; i < this->macGrid.numParticles; i++) {
+          // Get cell in which particle exists
+          Eigen::Ref<EigenVector> p = particles.segment<numElm>(i*numElm);
+          auto* cell = this->macGrid.getCellFromParticle(p);
+
+          // Check if cell is not within defined bounds
+          if (cell == nullptr) {
+            if (this->cellIsWithinSimBounds()) {
+              typename grid::MacGrid<EigenVector>::Cell newCell = { grid::CellTypes::FLUID, 0 };
+              this->macGrid[p] = cell;
+            }
+          } else if (this->cellIsInSolidBounds(cell)) {
+            cell->type = grid::CellTypes::FLUID;
+            cell->layer = 0;
+          }
+        }
+
+        // Create a buffer zone around the fluid
+        for (int i = 1; i < std::max(2, std::ceil(kcfl)); i++) {
+          for (auto& cell : this->macGrid) {
+            if (cell.layer != i-1) {
+              continue;
+            }
+            // Loop through neighbours of cell
+            auto neighbours = this->macGrid.generateNeighboursFromCell(cell);
+
+            // TODO: Update pointer connections
+            for (auto& n : neighbours) {
+              auto* nc = this->macGrid.getCellFromParticle(n);
+              if (nc != nullptr) {
+                if (nc->layer == -1 && nc->type != grid::CellTypes::SOLID) {
+                  nc->type = grid::CellTypes::AIR;
+                  nc->layer = i;
+                }
+              } else {
+                this->macGrid[nc] = grid::MacGrid<EigenVector>::Cell{
+                    i,
+                    this->cellIsWithinSimBounds() ? grid::CellTypes::AIR : grid::CellTypes::SOLID
+                };
+              }
+            }
+          }
+        }
+        // TODO: Delete any cell with layer == -1
+      }
+
+      void adjectVelocityField() {
+        for (auto& cell : this->macGrid) {
 
         }
+      }
+
+      EigenVector traceParticle(EigenVector& e) {
+        auto v = this->macGrid.interpolateVelocity(e);
+        auto v2 = this->macGrid.interpolateVelocity(e + v*0.5);
+
+        return e + v2 * this->dt;
+      }
+
+      bool cellIsInSolidBounds(){}
+      bool cellIsWithinSimBounds(){}
+    private:
+      EigenVector gravity;
+      double dt;
+      double density;
+      double vis;
+
+      // Simulation helper functions
+      inline void advect(Eigen::MatrixXd &q, Eigen::MatrixXd &qdot){
+      }
     };
 
-    //Simulation objects
-    GeneralSimulation water_sim;
-
+//Simulation objects
+GeneralSimulation<Eigen::Vector2d> water_sim;
 }
 
 #endif //A6_RIGID_BODIES_CONTACT_SIMULATION_H
